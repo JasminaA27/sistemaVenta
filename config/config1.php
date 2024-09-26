bd246c2c79f8923fcf69b2b261e054f75faacbb1
JasminaA27 <avalosyanet123@gmail.com>
2024-09-26 10:15:44 -0500

    a

--- config/config1.php
+++ config/config1.php
@@ -1,9 +1,9 @@
 <?php
 const BD_HOST = 'Localhost';
-const BD_NAME = 'sistema_venta';
-const BD_USER = 'root';
+const BD_NAME = '';
+const BD_USER = '';
 const BD_PASSWORD = '';
 
-const BASE_URL = 'http://localhost/sistemaVenta/';
+const BASE_URL = '';
 
 ?>
